57244498833544a0f5982561b58e6687a86a2c66
Ian Cruz <iankruz98@gmail.com>
2019-10-26 19:29:42 +1100

    fixing git brnaches

--- main.py
+++ main.py
@@ -5,70 +5,67 @@ from zipfile import ZipFile
 import os, shutil
 
 
-cwd = os.getcwd()
 
 ## Get the base name for the project and store it
 base_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
-def get_all_file_paths(directory):
-    file_paths = [] 
 
+def zipdir(path, zip_File):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zip_File.write(os.path.join(root, file))
+            print(files)
 
-    #crawl through folder directory
-    for root, directories, files in os.walk(directory):
-        for filename in files:
-            # Join two strings together to form full filepath
-            filepath = os.path.join(root, filename)
-            file_paths.append(filepath)
-
-    return file_paths
-
-
+def create_zip(dir, file_name):
+    zipf = ZipFile(dir + "\ " +  file_name, "w")
+    zipdir(dir, zipf)
+    zipf.close()
 
 def main():
     
 
     # path to folder which needs to be zipped 
-    directory = cwd
-    dst = cwd + base_name + "\Backup\ ".strip()
-    targetDir = '{}\{}\{}'.format(cwd, base_name,"Backup")
-    file_paths = get_all_file_paths(directory)
-    
-    print("DST: " + targetDir)
+    directory = os.getcwd()
+    dst = directory + base_name + "\Backup\ ".strip()
+
+    targetDir = '{}\{}\{}'.format(directory, base_name,"Backup")
+    dirLength = 0
 
     # Check if backup folder already exist
-    #if not -> create a backup folder
+    # if not -> create a backup folder
     if not os.path.exists(targetDir):
         os.mkdir(targetDir)
+
     else:   
-        print ("Path exists")
+        # print ("Path exists")
         dirList = os.listdir(targetDir)
-        dirListAmount = len(dirList)        
+        dirLength = len(dirList)   
 
-        # printing the list of all files to be zipped 
-        print('Following files will be zipped:') 
-        for file_name in file_paths: 
-            print(file_name) 
-            
-        # writing files to a zipfile     
-        # file name and number
-        f_num = str(dirListAmount).zfill(2)
-        f_name = '{}_{}{}'.format(base_name, f_num, ".zip")
+    # writing files to a zipfile     
+    # file name and number
+    f_num = str(dirLength).zfill(2)
+    f_name = '{}_{}{}'.format(base_name, f_num, ".zip")
 
-        print(f_name)
+    print("TARGETDIR: " + dst)        ###DEBUG
+
     
-        try:
-            with ZipFile(dst + f_name,'w') as zip: 
-            # writing each file one by one 
-                for file in file_paths: 
-                    zip.write(file) 
 
-            print('All files zipped successfully!')  
-        except:
-            print("Cannot zip files!")
+   
+        
+
+        
+    
+    try:
+        create_zip(targetDir, f_name)
+        print("ZipFile created Successfully!")
+    except FileExistsError:
+        print("Cannot Find File")
+        return
   
+
     
 
-
+# run Main function
 if __name__ == "__main__": 
     main() 
